326e636d8772c67cf8e1c9f12b9bcddc453607ac
ForAeons <w.xy020203@gmail.com>
2024-05-06 14:49:34 +0800

    Add deploy script to run ngrok

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Load environment variables
+export $(cat ./lms-backend/.env.development | xargs)
+export $(cat .env.local | xargs)
+
+echo "Building Docker image..."
+docker build .
+
+# Build and start Docker containers
+echo "Starting Docker containers..."
+docker compose up -d
+
+# Wait for the server to start
+echo "Waiting for the server to become active..."
+while ! curl -s http://localhost:$PORT > /dev/null; do
+    echo "Waiting for server to respond..."
+    sleep 5
+done
+echo "Server is up and running at http://localhost:$PORT"
+
+# Start ngrok
+echo "Starting ngrok..."
+ngrok config add-authtoken $NGROK_AUTHTOKEN
+ngrok http --domain $NGROK_DOMAIN $PORT 
